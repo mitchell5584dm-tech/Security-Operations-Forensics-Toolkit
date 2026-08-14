c58e9a08d75ecd18d8aa48ff520eb5768ed9f2cf
David <mitchell5584.dm@gmail.com>
2026-08-14 13:28:40 -0400

    Change index route to serve index_render.html

--- webapp/app.py
+++ webapp/app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return send_from_directory('..', 'index.html')
+    return send_from_directory('.', 'index_render.html')
 
 @app.route('/ping')
 def ping():
